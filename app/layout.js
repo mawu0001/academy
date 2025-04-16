052991c77269527eecd141e982193b372b928b42
Marie Wulff <mawu0001@stud.kea.dk>
2025-04-16 10:02:26 +0200

    no index added

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import Layoutwrapper from "./components/Layoutwrapper.jsx";
 
 export const metadata = {
   title: "Gastrotools Product Academy",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({ children }) {
